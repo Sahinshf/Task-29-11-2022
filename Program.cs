ba7c4eeacd31e854fb030be7f3bd6d19023a2be0
Sahinshf <shahinrsha@code.edu.az>
2022-11-29 17:10:16 +0400

    Task is here

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { //Program.cs Main
+
+            Console.WriteLine("Did he/she graduated ?");
+            string graduated = Console.ReadLine();
+            Console.WriteLine("Please enter information: ");
+
+            if (graduated=="yes" || graduated == "Yes" || graduated == "YES")
+            {
+                Student student_info = new Student(Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), Convert.ToByte(Console.ReadLine()) , true);
+
+                student_info.name_surname();
+                student_info.all_information();
+                Console.WriteLine("This student has graduated");
+                student_info.second_chance();
+            }
+
+            else
+            {
+                Student student_info = new Student(Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), Convert.ToByte( Console.ReadLine() ) , false );
+
+                student_info.name_surname();
+                student_info.all_information();
+                Console.WriteLine(" This student didn`t graduate");
+                student_info.second_chance();
+            }
+            
+
+
+
+
+
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Student
+    { // Class
+
+        public string Name;
+        public string Surname;
+        public string Group;
+        public byte Point;
+        public bool Graduated = true;
+
+        public Student(string name, string surname, string group, byte point, bool graduated) : this(point, graduated)
+        {
+            Name = name;
+            Surname = surname;
+            Group = group;
+        }
+
+        public Student(byte Point, bool graduated) : this(graduated)
+        {
+            this.Point = Point;
+        }
+
+        public Student(bool graduated)
+        {
+            Graduated = graduated;
+        }
+
+        public void name_surname()
+        {
+            Console.WriteLine(Name + " " + Surname);
+        }
+
+
+        public void all_information()
+        {
+            Console.Write(Name + " " + Surname + " " + Group + " " + Point + " =>");
+            
+        }
+
+        public void second_chance()
+        {
+            if(Point>80)
+            {
+                Console.WriteLine("You have a second chance");
+            }
+            else
+            {
+                Console.WriteLine("Sorry , You don't get a second chance :/");
+            }
+        }
+    }
+}
+ 
